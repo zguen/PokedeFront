38498a921bb8f36ea8ae169fcf7e7e0c39eef091
zguen <guillaume.mousin@gmail.com>
2023-10-11 20:49:52 +0200

    debut création composants et routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { PokemonsComponent } from './pages/pokemons/pokemons.component';
+import { PersonalComponent } from './pages/personal/personal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'pokemons', component: PokemonsComponent },
+  { path: 'personal', component: PersonalComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { PokemonsComponent } from './pages/pokemons/pokemons.component';
+import { PersonalComponent } from './pages/personal/personal.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    PokemonsComponent,
+    PersonalComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
